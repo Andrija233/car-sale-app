88997b5ff9f9f671d7c089da8e6022a03f710362
Andrija233 <andrijam007@hotmail.rs>
2025-03-20 01:02:41 +0100

    Added Controller

--- car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+644a48c7914f67c829b23ef00a756e4dec8fb305")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2749f0af8c0053bd232d335703501feef4e01de")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
