0e2530f87263520b37fa626bb86cf32daf2e7191
Andrija233 <andrijam007@hotmail.rs>
2025-03-23 12:12:10 +0100

    Added identity service project

--- car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52710335dee3eea0d25db4eedb2c9b4f2eead872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad417429ff4e3acf5fe268a6eccc7fb75bfb5d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
