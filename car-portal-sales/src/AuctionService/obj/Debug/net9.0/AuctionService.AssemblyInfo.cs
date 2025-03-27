9faf0fdd1403df9bb6856d7d8b2f158e095a70c4
Andrija233 <andrijam007@hotmail.rs>
2025-03-27 17:30:15 +0100

    Bug fixes

--- car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bd62758899850b2601c6bde4ea007bd2bea949")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18867ab287b294b4bd62eaec5c3ba86fa7d4806a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
