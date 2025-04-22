f192d27f1d240096e448482b823f71e63b3ba77b
Andrija233 <andrijam007@hotmail.rs>
2025-04-22 13:06:07 +0200

    Added certs for https

--- car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
+++ car-portal-sales/src/AuctionService/obj/Debug/net9.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+503060916db3537580605291b95a404ae07df8f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65aa51971d4b7b06fc588570f58b9ca276cd16c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
